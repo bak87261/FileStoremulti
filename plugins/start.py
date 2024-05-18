09455328dc54817ec0d10d199ea45a89c141c81f
actchanthar <159926023+actchanthar@users.noreply.github.com>
2024-05-18 10:36:25 +0630

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,8 @@ async def start_command(client: Client, message: Message):
     
     [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                    InlineKeyboardButton('👩‍💻 Dev', url='https://t.me/When_the_night_falls_my_soul_sea')
+                
         
     ]
             ]
